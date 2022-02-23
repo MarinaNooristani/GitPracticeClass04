069bcf62766c00892494db5a9250de577cfe8b48
MarinaNooristani <marina.nooristani321@gmail.com>
2022-02-23 16:23:32 +0300

    2nd commit

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -4,6 +4,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi this is git class");
+		System.out.println("lets make some changes");
 	}
 
 }
